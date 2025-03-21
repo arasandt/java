1f459ceebaad68bf377a992b1125b66a5d8dbbe8
Arasan Dayalan <arasan_dt@yahoo.co.in>
2025-03-21 14:50:02 +0000

    added Online-Banking-System and shopizer

---
+++ WorkingJava8/Online-Banking-System/src/main/java/com/userfront/service/UserServiceImpl/UserServiceImpl.java
@@ -0,0 +1,124 @@
+package com.userfront.service.UserServiceImpl;
+
+import java.util.List;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.userfront.dao.RoleDao;
+import com.userfront.dao.UserDao;
+import com.userfront.domain.User;
+import com.userfront.domain.security.UserRole;
+import com.userfront.service.AccountService;
+import com.userfront.service.UserService;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService{
+	
+	private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+	
+	private final UserDao userDao;
+    private final RoleDao roleDao;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final AccountService accountService;
+
+    public UserServiceImpl(UserDao userDao, RoleDao roleDao, BCryptPasswordEncoder passwordEncoder, @Lazy AccountService accountService) {
+        this.userDao = userDao;
+        this.roleDao = roleDao;
+        this.passwordEncoder = passwordEncoder;
+        this.accountService = accountService;
+    }    
+	
+	public void save(User user) {
+        userDao.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userDao.findByUsername(username);
+    }
+
+    public User findByEmail(String email) {
+        return userDao.findByEmail(email);
+    }
+    
+    
+    public User createUser(User user, Set<UserRole> userRoles) {
+        User localUser = userDao.findByUsername(user.getUsername());
+
+        if (localUser != null) {
+            LOG.info("User with username {} already exist. Nothing will be done. ", user.getUsername());
+        } else {
+            String encryptedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encryptedPassword);
+
+            for (UserRole ur : userRoles) {
+                roleDao.save(ur.getRole());
+            }
+
+            user.getUserRoles().addAll(userRoles);
+
+            user.setPrimaryAccount(accountService.createPrimaryAccount());
+            user.setSavingsAccount(accountService.createSavingsAccount());
+
+            LOG.info("Saving user: {}", user.getUsername());
+            localUser = userDao.save(user);
+            LOG.info("User saved successfully: {}", localUser.getUsername());
+
+            // localUser = userDao.save(user);
+        }
+
+        return localUser;
+    }
+    
+    public boolean checkUserExists(String username, String email){
+        if (checkUsernameExists(username) || checkEmailExists(username)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean checkUsernameExists(String username) {
+        if (null != findByUsername(username)) {
+            return true;
+        }
+
+        return false;
+    }
+    
+    public boolean checkEmailExists(String email) {
+        if (null != findByEmail(email)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public User saveUser (User user) {
+        return userDao.save(user);
+    }
+    
+    public List<User> findUserList() {
+        return userDao.findAll();
+    }
+
+    public void enableUser (String username) {
+        User user = findByUsername(username);
+        user.setEnabled(true);
+        userDao.save(user);
+    }
+
+    public void disableUser (String username) {
+        User user = findByUsername(username);
+        user.setEnabled(false);
+        System.out.println(user.isEnabled());
+        userDao.save(user);
+        System.out.println(username + " is disabled.");
+    }
+}
